f65ff6a65fdfaa022bcdd560be3e3b82fcef16cb
nickshamrock <nickshamrock1997@gmail.com>
2024-03-28 15:48:11 +0300

    новый коммит

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
 const numbers = [1, 2, 3, 3, 4, 4];
 const results = numbers.map(number => number * 2); 
 console.log(results);
+
+//переписать в стрелочную функцию 
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Вы согласны?",
+    function() { alert("Вы согласились."); },
+    function() { alert("Вы отменили выполнение."); }
+  );
+
+//стрелочная функция из задачи выше
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert("Вы отменили выполнение.")
+  );
+
+// пример стрелочной функции 
+
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome = (age < 18) ?
+  () => alert('Привет!') :
+  () => alert("Здравствуйте!");
+
+welcome();
+
+
+//пример стрелочной функции
+
+  let sum = (a, b) => a + b;
+
+/* Эта стрелочная функция представляет собой более короткую форму:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
